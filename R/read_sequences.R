3cb9ed361464a4314d8ed24a70c5bf1fd350ff49
Baldribert <baldribert1@gmail.com>
2020-01-22 22:03:00 +0100

    added alignment

--- R/read_sequences.R
+++ R/read_sequences.R
@@ -4,3 +4,7 @@ library(msa)
 mySequences <- readAAStringSet("./data/seqData.fasta")
 
 print(mySequences)
+
+myFirstAlignment <- msa(mySequences)
+
+msaPrettyPrint(myFirstAlignment, output="pdf", showNames="none",showLogo="none", askForOverwrite=FALSE, verbose=FALSE)
